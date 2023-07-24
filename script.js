50810b8226232e2c504328a353a60914087ff2e5
TrickyLiam <liamcarlson2014@gmail.com>
2023-07-24 15:24:46 -0500

    Initial commit

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+   fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        return response.json();
+    }).then(function (data){
+        // console.log(data);
+        const container = document.getElementById("container");
+        // const astronaut = document.querySelector("astronaut");
+        // const bio = document.querySelector("bio");
+    
+        data.forEach(astronaut => {
+            // console.log(astronaut);        
+            // console.log(astronaut.firstName);
+                    
+        });
+    
+
+
+    });
+
+   
+});
