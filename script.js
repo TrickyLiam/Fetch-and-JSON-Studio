342430b98d49f788ccdff16196816fc9a2ee81ff
TrickyLiam <liamcarlson2014@gmail.com>
2023-07-24 17:46:31 -0500

    Finish Studio

--- script.js
+++ script.js
@@ -10,12 +10,24 @@ window.addEventListener("load", function() {
         data.forEach(astronaut => {
             // console.log(astronaut);        
             // console.log(astronaut.firstName);
-                    
+            container.innerHTML += `
+            <div id="container">
+            <div class="astronaut">
+            <div class="bio">
+            <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+            <ul>
+            <li>Hours in space: ${astronaut.hoursInSpace} </li>
+            <li>Active: ${astronaut.active}</li>
+            <li>Skills: ${astronaut.skills}</li>
+            </ul>
+            </div>
+            <img src="${astronaut.picture}" class="avatar"/>
+            </div>
+            </div>
+            `;
         });
     
+        // container.innerHTML = `<h3>${data[0].firstName}</h3>`;
 
-
-    });
-
-   
+    }); 
 });
